4eb99f9c4123b32da39ed3e378fb77beaffff154
Luca Di Vittorio <luca.div@hotmail.com>
2022-06-23 13:26:06 +0100

    added private methods scan and total at the checkout

--- checkout.rb
+++ checkout.rb
@@ -8,6 +8,37 @@ class Checkout
     @spending_deal = args["spending_deal"]
   end
 
+  private
 
+  def scan(item)
+    if !@prices.keys.include? item
+      puts " "
+      return puts "Error this item is not in the list, try again."
+      puts " "
+    end
+    if @basket.any? do |product|
+        product["code"] == item
+      end
+      find_item = @basket.detect do |product|
+        product["code"] == item
+      end
+      index = @basket.index(find_item)
+      @basket[index]["quantity"] += 1
+    else
+      @basket << {"code" => item, "quantity" => 1}
+    end
+  end
 
+  def total
+    @basket.each_with_index do |product, index|
+      @prices[product["code"]]["price"].each do |key, value|
+        if product["quantity"] >= key.to_i
+          @total += (product["quantity"] * value)
+          break
+        end
+      end
+    end
+    total = @total > @spending_deal["min_spend"] ? 0.01 * @total * (100 - @spending_deal["percent_off"]) : @total
+    total.round(2)
+  end
 end
