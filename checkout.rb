88056692be5a31b643b046abd114886b9f2b0167
Luca Di Vittorio <luca.div@hotmail.com>
2022-06-23 14:40:53 +0100

    completed the interface for the checkout

--- checkout.rb
+++ checkout.rb
@@ -1,44 +1,44 @@
 class Checkout
-  attr_reader :total, :basket, :prices, :spending_deal
+  attr_reader :total, :basket, :items, :spending_deal
 
   def initialize(args = {})
     @total = 0
     @basket = []
-    @prices = args["prices"]
-    @spending_deal = args["spending_deal"]
+    @items = args['items']
+    @spending_deal = args['spending_deal']
   end
 
   private
 
+  # If an item is already in the basket add one quantity otherwise add item
   def scan(item)
-    if !@prices.keys.include? item
-      puts " "
-      return puts "Error this item is not in the list, try again."
-      puts " "
+    if !@items.keys.include? item
+      puts 'Error this item is not in the list, try again.'
     end
     if @basket.any? do |product|
-        product["code"] == item
+        product['code'] == item
       end
       find_item = @basket.detect do |product|
-        product["code"] == item
+        product['code'] == item
       end
       index = @basket.index(find_item)
-      @basket[index]["quantity"] += 1
+      @basket[index]['quantity'] += 1
     else
-      @basket << {"code" => item, "quantity" => 1}
+      @basket << {'code' => item, 'quantity' => 1}
     end
   end
 
-  def total
+  # Apply discount in case total is more than the minimum spending discount
+  def total_after_discount
     @basket.each_with_index do |product, index|
-      @prices[product["code"]]["price"].each do |key, value|
-        if product["quantity"] >= key.to_i
-          @total += (product["quantity"] * value)
+      @items[product['code']]['price'].each do |key, value|
+        if product['quantity'] >= key.to_i
+          @total += (product['quantity'] * value)
           break
         end
       end
     end
-    total = @total > @spending_deal["min_spend"] ? 0.01 * @total * (100 - @spending_deal["percent_off"]) : @total
-    total.round(2)
+    total_after_discount = @total > @spending_deal['min_spend'] ? 0.01 * @total * (100 - @spending_deal['percent_off']) : @total
+    total_after_discount.round(2)
   end
 end
--- interface.rb
+++ interface.rb
@@ -1,20 +1,72 @@
 # This require relative is to call the checkout file
-require_relative "./checkout"
+require_relative './checkout'
 
-### Talking with the user ###
-puts "add an item to the basket"
+# Items to pass as an argument to the checkout
+promotional_rules = {
+  'items' => {
+    '001' => {
+      'name' => 'lavander heart',
+      'price' => {
+        '2' => 8.5,
+        '1' => 9.25
+      }
+    },
+    '002' => {
+      'name' => 'personalized cufflinks',
+      'price' => {
+        '1' => 45.00
+      }
+    },
+    '003' => {
+      'name' => 'kids t-shirt',
+      'price' => {
+        '1' => 19.95
+      }
+    }
+  },
+  'spending_deal' => {
+    'min_spend' => 60,
+    'percent_off' => 10
+  }
+}
 
+# We initialize a new checkout with the promotional rules
+co = Checkout.new(promotional_rules)
 
-puts "add another item to the basket"
+# we show what items and possible promotions on quantity we have
+puts 'Items in stock:'
+promotional_rules['items'].each do |key, value|
+  puts ' '
+  puts "Code: #{key}"
+  puts "Name: #{value['name']}"
+  value['price'].each do |k, v|
+    puts "buy #{k} #{value['name']} for #{v}"
+  end
+end
 
+# We ask to select items to add to the basket
+puts ' '
+puts 'Select an item to scan'
 
-puts "add one last item to the basket"
+item = gets.chomp
+co.scan(item)
 
-#############################
+# We keep asking until they press enter with an empty string
+while item != ''
+  puts 'Select an item to scan or press enter to proceed to the checkout'
+  item = gets.chomp
+  co.scan(item) if item != ''
+end
 
-puts "Computing your total (checking for discounts available)........"
+# Creating an empty basket and pushing any single item in it
+basket = []
+co.basket.each do |item|
+  item['quantity'].times { basket << item['code'] }
+end
 
-# Using the method to calculate the total!
-
-
-# Finally, we print the total including possible promotions:
+# Displaying total before discount, then if the basket is empty
+# we either return the empty basket string or the items and quantity
+# at last we display the final price in case the 10% has been added
+puts "total price before discount: £#{co.total}"
+puts "basket: #{basket.length.zero? ? 'basket is empty' : basket.join(', ')}"
+puts "Total price is: £#{total_after_discount}"
